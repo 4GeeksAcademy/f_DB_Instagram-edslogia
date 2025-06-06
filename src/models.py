82925db5d8aa8803e03983c4ce49a8091d6609a8
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-06-06 15:26:33 +0000

    tablas relacionadas

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey, Enum
 from sqlalchemy.orm import Mapped, mapped_column
+from eralchemy2 import render_er
 
 db = SQLAlchemy()
 
@@ -17,3 +18,55 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Follower(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id
+        }
+
+class Post(db.Model):    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+
+    def serialize(self):
+        return{
+            "user_id": self.user_id
+        }
+    
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(180))
+    author_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'))
+
+    def serialize(self):
+        return {
+            "comment_text": self.comment_text,
+            "author_id": self.author_id,
+            "post_id": self.post_id
+        }
+    
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(Enum('image', 'video', name='media_type'), nullable=False)
+    url: Mapped[str] = mapped_column(unique=True, nullable=False)
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'))
+
+    def serialize(self):
+        return{ 
+            "type" : self.type,
+            "url" : self.url,
+            "post_id" : self.post_id
+        }
+
+try:
+    render_er(db.Model, 'diagram.png')
+    print("✅ Diagrama generado correctamente como diagram.png")
+except Exception as e:
+    print("❌ Error generando el diagrama:", e)
