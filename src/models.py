039a26cae5ab5405bbf14aa44019a242f73df473
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-06-06 16:43:57 +0000

    se agregan relationship y list, proyecto finalizado

--- src/models.py
+++ src/models.py
@@ -1,18 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, ForeignKey, Enum, Date, Time, DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Enum, Date, Time, DateTime, Integer, Numeric
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from eralchemy2 import render_er
 from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    password: Mapped[str] = mapped_column(String(16),nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-    country: Mapped[str] = mapped_column()
-    gender: Mapped[str] = mapped_column(nullable=False)
+    country: Mapped[str] = mapped_column(String(80))
+    gender: Mapped[str] = mapped_column(String(80),nullable=False)
+    posts: Mapped[list["Post"]] = relationship("Post", back_populates = "user")
 
     def serialize(self):
         return {
@@ -21,11 +22,24 @@ class User(db.Model):
             "gender": self.gender,
             "country": self.country
         }
+    
+class Post(db.Model):    
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer(), ForeignKey('user.id'))
+    date_time: Mapped[datetime] = mapped_column(DateTime(), nullable=False)
+    user: Mapped["User"] = relationship("User", back_populates="post")
+    comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="post_comments")
+
+    def serialize(self):
+        return{
+            "user_id": self.user_id,
+            "date_time": self.date_time
+        }
 
 class Follower(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
-    user_to_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(Integer(), ForeignKey('user.id'), nullable=False)
+    user_to_id: Mapped[int] = mapped_column(Integer(), ForeignKey('user.id'), nullable=False)
 
     def serialize(self):
         return {
@@ -33,24 +47,15 @@ class Follower(db.Model):
             "user_to_id": self.user_to_id
         }
 
-class Post(db.Model):    
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
-    date_time: Mapped[datetime] = mapped_column(DateTime(), nullable=False)
-
-    def serialize(self):
-        return{
-            "user_id": self.user_id,
-            "date_time": self.date_time
-        }
-    
+  
 class Comment(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
     comment_text: Mapped[str] = mapped_column(String(180))
-    author_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
-    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'))
+    author_id: Mapped[int] = mapped_column(Integer(), ForeignKey('user.id'))
+    post_id: Mapped[int] = mapped_column(Integer(), ForeignKey('post.id'))
     date: Mapped[datetime.date] = mapped_column(Date(), nullable=False)
     time: Mapped[datetime.time] = mapped_column(Time(), nullable=False)
+    post_comments: Mapped["Post"] = relationship("Post", back_populates="comments")
 
     def serialize(self):
         return {
@@ -62,16 +67,18 @@ class Comment(db.Model):
         }
     
 class Media(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(Integer(), primary_key=True)
     type: Mapped[str] = mapped_column(Enum('image', 'video', name='media_type'), nullable=False)
-    url: Mapped[str] = mapped_column(unique=True, nullable=False)
-    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'))
+    url: Mapped[str] = mapped_column(String(), unique=True, nullable=False)
+    post_id: Mapped[int] = mapped_column(Integer(), ForeignKey('post.id'))
+    size_mb: Mapped[float] = mapped_column(Numeric(10,2), nullable=False)
 
     def serialize(self):
         return{ 
             "type" : self.type,
             "url" : self.url,
-            "post_id" : self.post_id
+            "post_id" : self.post_id,
+            "size_mb" : self.size_mb
         }
 
 try:
